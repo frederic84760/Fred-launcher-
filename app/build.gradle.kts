39e2e6b68e9972e3e963d020bda72c5a0543f8f2
frederic <117153280+frederic84760@users.noreply.github.com>
2026-09-14 16:25:51 +0200

    Downgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "fr.fred.launcher"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "fr.fred.launcher"
         minSdk = 29
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
