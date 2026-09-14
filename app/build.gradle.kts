43b7ef838fe157c9b859993dc2a499901a1240d9
frederic <117153280+frederic84760@users.noreply.github.com>
2026-09-14 23:18:36 +0200

    Update dependencies to include composeBom in implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,9 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.04.01")
-    androidTestImplementation(composeBom)
+val composeBom = platform("androidx.compose:compose-bom:2026.04.01")
+implementation(composeBom)
+androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.13.0")
