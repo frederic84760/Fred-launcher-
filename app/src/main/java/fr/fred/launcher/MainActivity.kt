02fbe931988fd9fe3305377a3123900e60a50568
frederic <117153280+frederic84760@users.noreply.github.com>
2026-09-15 13:02:20 +0200

    Fix package declaration formatting in MainActivity.kt

--- app/src/main/java/fr/fred/launcher/MainActivity.kt
+++ app/src/main/java/fr/fred/launcher/MainActivity.kt
@@ -1,4 +1,4 @@
-package fr.fred.launcher
+įpackage fr.fred.launcher
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
@@ -30,7 +30,6 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
